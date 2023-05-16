f645c22522c83f3af404cceb7e6f80e0075b9ccd
Xiaoyan(Sharon) MU <xmu@esri.com>
2023-05-16 19:26:51 +0800

    Site updated: 2023-05-16 19:26:36

--- js/totop.js
+++ js/totop.js
@@ -1,11 +1,12 @@
 $(window).scroll(function() {
-    $(window).scrollTop() > 500 ? $("#rocket").addClass("show") : $("#rocket").removeClass("show");
+    $(window).scrollTop() > $(window).height()*0.5 ? $("#rocket").addClass("show") : $("#rocket").removeClass("show");
 });
+
 $("#rocket").click(function() {
     $("#rocket").addClass("launch");
     $("html, body").animate({
         scrollTop: 0
-    }, 500, function() {
+    }, 1000, function() {
         $("#rocket").removeClass("show launch");
     });
     return false;
